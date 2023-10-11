de5a19a0f1e16eeb6ee631dca56cdd5dd431c076
juanjosegdoj <juanjosegdoj@gmail.com>
2023-10-10 22:59:16 -0500

    Adding backend in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
-    proxy: 'http://ec2-35-171-243-24.compute-1.amazonaws.com:800'
+    proxy: 'http://ec2-35-171-243-24.compute-1.amazonaws.com:8000'
   }
 }
